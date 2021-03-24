255806ed21d64950f33e8c66d112da729f5f6e67
Luis G. Villegas <luisguve@gmail.com>
2021-03-24 17:55:53 -0300

    Initial commit

---
+++ js/play.js
@@ -0,0 +1,230 @@
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
+var conn;
+
+var board = null
+var game = new Chess()
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+var finished = true
+var gameId = getUrlParameter("id")
+var color = getUrlParameter("color")
+
+var log = document.getElementById("log");
+
+function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+  return false;
+}
+
+if (window["WebSocket"]) {
+  conn = new WebSocket(`ws://localhost:8000/game?id=${gameId}`);
+  conn.onclose = function (evt) {
+    let item = document.createElement("b");
+    item.innerHTML = "Connection closed.";
+    appendLog(item);
+  };
+  conn.onmessage = function (evt) {
+    let move = game.move(JSON.parse(evt.data));
+
+    // see if the move is legal
+    if (move === null) {
+      let item = document.createElement("b");
+      item.innerHTML = `Error: illegal move: ${evt.data}`;
+      appendLog(item);
+      return;
+    }
+
+    board.position(game.fen());
+    updateStatus();
+  };
+} else {
+  alert("Your browser does not support WebSockets.");
+}
+
+function appendLog(item) {
+  log.appendChild(item);
+}
+
+$('#3min').click(() => {
+  if (!finished) {
+    alert("You are in the middle of a game")
+    return
+  }
+  play(3);
+});
+
+$('#5min').click(() => {
+  if (!finished) {
+    alert("You are in the middle of a game")
+    return
+  }
+  play(5);
+});
+
+$('#10min').click(() => {
+  if (!finished) {
+    alert("You are in the middle of a game")
+    return
+  }
+  play(10);
+});
+
+function play(min) {
+  const url = `http:localhost:8000/play?clock=${min}`;
+  fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      $('#play-error').html("Could not reach server");
+    } else {
+      response.json();
+    }
+  })
+  .then(link => {
+    // redirect to play room
+    document.location.href = link;
+  });
+}
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && color === 'black') ||
+      (game.turn() === 'b' && color === 'white')) {
+    return false
+  }
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
+}
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  if (!conn) {
+    return 'snapback';
+  }
+
+  conn.send(JSON.stringify({
+    from: source,
+    to: target,
+    color: move.color,
+    promotion: 'q'
+  }));
+
+  updateStatus()
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+function updateStatus () {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // stalemate?
+  else if (game.in_stalemate()) {
+    status = 'Game over, ' + moveColor + ' is in stalemate.'
+  }
+
+  // prince promoted?
+  else if (game.prince_promoted()) {
+    moveColor = game.turn() === 'b' ? 'White' : 'Black'
+    status = 'Game over, ' + moveColor + ' promoted his prince.'
+  }
+
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+
+  $status.html(status)
+  // $fen.html(game.fen())
+  var pgnResult = game.pgn()
+      .split(/\d+\./)
+      .slice(1)
+      .map((steps, idx) => {
+        let history = steps.trim().split(/\s/)
+        let colspan = 1
+        if (history.length === 1) {
+          colspan = 2
+        }
+        history = history.reduce((res, step, idx, arr) => {
+          return res + `<td colspan="${colspan}">${step}</td>`
+        }, "")
+        return `
+          <tr>
+            <th scope="row" class="w-25">${idx + 1}</th>
+            ${history}
+          </tr>`
+      })
+      .reduce((res, steps) => { return res + steps }, `
+        <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col" class="w-25"></th>
+            <th scope="col">White</th>
+            <th scope="col">Black</th>
+          </tr>
+        </thead>
+        <tbody>`) + "</tbody>"
+  $pgn.html(pgnResult)
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  orientation: color,
+  onDrop,
+  onSnapEnd,
+  onDragStart
+}
+board = Chessboard('board', config)
+
+updateStatus()
